33b8d5f030ed424443ce13e106a3e83347eea476
Milton Chowdhury <miltonk875@gmail.com.com>
2024-12-11 12:34:46 +0600

    RAW SQL Query Update

---
+++ 1-Order-Quantity-Query-9-12-24/OrderQuantityRawQuery_9-12-2024.sql
@@ -0,0 +1,29 @@
+
+-- Total Orders By Given Today Date
+-- Default Type= sell, Other=
+SELECT COUNT(*) AS today_orders FROM transactions WHERE type ='sell' AND DATE(created_at) = '2024-12-10';
+
+-- Total Orders By Given Date (Yesterday)
+SELECT COUNT(*) AS yesterday_orders FROM transactions WHERE type ='sell' AND DATE(created_at) = '2024-12-08';
+
+-- Total Orders By This Week (From :last_seven_day to :yesterday)
+SELECT COUNT(*) AS week_orders FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-02' AND '2024-12-08';
+
+-- Total Orders By This Month (From :first_day_of_month to :to_month_date)
+SELECT COUNT(*) AS month_orders FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-01' AND '2024-12-31';
+
+-- Get Average Orders
+SELECT ROUND(COUNT(*) / 7) AS average_orders FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-02' AND '2024-12-08';
+
+-- Get Percentage Change for Today's Orders vs. Average Orders for the Week
+
+SELECT 
+    IFNULL(
+        ROUND(
+            (
+                (SELECT COUNT(*) FROM transactions WHERE type ='sell' AND DATE(created_at) = '2024-12-09') - 
+                (SELECT COUNT(*) / 7 FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-02' AND '2024-12-08')
+            ) / 
+            (SELECT COUNT(*) / 7 FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-02' AND '2024-12-08') * 100, 2
+        ), 
+    0) AS percentage_change;
---
+++ 3-Order-Query-Without-Sale-Return-11-12-24/total_order_amount_query_without_sale_return.sql
@@ -0,0 +1,29 @@
+
+-- Total amounts By Given Today Date
+-- Default Type= sell
+SELECT SUM(final_total) AS today_amounts FROM transactions WHERE type ='sell' AND DATE(created_at) = '2024-12-10' AND transactions.id NOT IN (SELECT return_parent_id FROM transactions WHERE return_parent_id IS NOT NULL);
+
+-- Total amounts By Given Date (Yesterday)
+SELECT SUM(final_total) AS yesterday_amounts FROM transactions WHERE type ='sell' AND DATE(created_at) = '2024-12-10' AND transactions.id NOT IN (SELECT return_parent_id FROM transactions WHERE return_parent_id IS NOT NULL);
+
+-- Total amounts By This Week (From :last_seven_day to :yesterday)
+SELECT SUM(final_total) AS week_amounts FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-03' AND '2024-12-10' AND transactions.id NOT IN (SELECT return_parent_id FROM transactions WHERE return_parent_id IS NOT NULL);
+
+-- Total amounts By This Month (From :first_day_of_month to :to_month_date)
+SELECT SUM(final_total) AS month_amounts FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-01' AND '2024-12-31' AND transactions.id NOT IN (SELECT return_parent_id FROM transactions WHERE return_parent_id IS NOT NULL);
+
+-- Get Average amounts
+SELECT ROUND(SUM(final_total) / 7) AS average_amounts FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-03' AND '2024-12-10' AND transactions.id NOT IN (SELECT return_parent_id FROM transactions WHERE return_parent_id IS NOT NULL);
+
+-- Get Percentage Change for Today's amounts vs. Average amounts for the Week
+
+SELECT 
+    IFNULL(
+        ROUND(
+            (
+                (SELECT SUM(final_total) FROM transactions WHERE type ='sell' AND DATE(created_at) = '2024-12-09' AND transactions.id NOT IN (SELECT return_parent_id FROM transactions WHERE return_parent_id IS NOT NULL)) - 
+                (SELECT SUM(final_total) / 7 FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-03' AND '2024-12-10' AND transactions.id NOT IN (SELECT return_parent_id FROM transactions WHERE return_parent_id IS NOT NULL))
+            ) / 
+            (SELECT SUM(final_total) / 7 FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-03' AND '2024-12-10' AND transactions.id NOT IN (SELECT return_parent_id FROM transactions WHERE return_parent_id IS NOT NULL)) * 100, 2
+        ), 
+    0) AS percentage_change;
