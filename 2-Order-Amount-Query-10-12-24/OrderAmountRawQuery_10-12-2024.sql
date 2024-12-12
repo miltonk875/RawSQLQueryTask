641be013e9cc54466afb63b9c0a08973cff1530a
Milton Chowdhury <miltonk875@gmail.com.com>
2024-12-12 11:23:25 +0600

    transaction_date update

--- 2-Order-Amount-Query-10-12-24/OrderAmountRawQuery_10-12-2024.sql
+++ 2-Order-Amount-Query-10-12-24/OrderAmountRawQuery_10-12-2024.sql
@@ -1,19 +1,19 @@
 
 -- Total amounts By Given Today Date
 -- Default Type= sell
-SELECT SUM(final_total) AS today_amounts FROM transactions WHERE type ='sell' AND DATE(created_at) = '2024-12-10';
+SELECT SUM(final_total) AS today_amounts FROM transactions WHERE type ='sell' AND DATE(transaction_date) = '2024-12-10';
 
 -- Total amounts By Given Date (Yesterday)
-SELECT SUM(final_total) AS yesterday_amounts FROM transactions WHERE type ='sell' AND DATE(created_at) = '2024-12-10';
+SELECT SUM(final_total) AS yesterday_amounts FROM transactions WHERE type ='sell' AND DATE(transaction_date) = '2024-12-10';
 
 -- Total amounts By This Week (From :last_seven_day to :yesterday)
-SELECT SUM(final_total) AS week_amounts FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-03' AND '2024-12-10';
+SELECT SUM(final_total) AS week_amounts FROM transactions WHERE type ='sell' AND transaction_date BETWEEN '2024-12-03' AND '2024-12-10';
 
 -- Total amounts By This Month (From :first_day_of_month to :to_month_date)
-SELECT SUM(final_total) AS month_amounts FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-01' AND '2024-12-31';
+SELECT SUM(final_total) AS month_amounts FROM transactions WHERE type ='sell' AND transaction_date BETWEEN '2024-12-01' AND '2024-12-31';
 
 -- Get Average amounts
-SELECT ROUND(SUM(final_total) / 7) AS average_amounts FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-03' AND '2024-12-10';
+SELECT ROUND(SUM(final_total) / 7) AS average_amounts FROM transactions WHERE type ='sell' AND transaction_date BETWEEN '2024-12-03' AND '2024-12-10';
 
 -- Get Percentage Change for Today's amounts vs. Average amounts for the Week
 
@@ -21,9 +21,9 @@ SELECT
     IFNULL(
         ROUND(
             (
-                (SELECT SUM(final_total) FROM transactions WHERE type ='sell' AND DATE(created_at) = '2024-12-09') - 
-                (SELECT SUM(final_total) / 7 FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-03' AND '2024-12-10')
+                (SELECT SUM(final_total) FROM transactions WHERE type ='sell' AND DATE(transaction_date) = '2024-12-09') - 
+                (SELECT SUM(final_total) / 7 FROM transactions WHERE type ='sell' AND transaction_date BETWEEN '2024-12-03' AND '2024-12-10')
             ) / 
-            (SELECT SUM(final_total) / 7 FROM transactions WHERE type ='sell' AND created_at BETWEEN '2024-12-03' AND '2024-12-10') * 100, 2
+            (SELECT SUM(final_total) / 7 FROM transactions WHERE type ='sell' AND transaction_date BETWEEN '2024-12-03' AND '2024-12-10') * 100, 2
         ), 
     0) AS percentage_change;
