e9a198b20b7607cb55d3325a666d6c48bf128a20
Milton Chowdhury <miltonk875@gmail.com.com>
2025-03-13 15:55:43 +0600

    customers query

--- Customers/5_customer_list.sql
+++ Customers/5_customer_list.sql
@@ -3,36 +3,53 @@ SELECT
     ad.id AS customer_id,  
     MIN(ad.name) AS customer_name,  
     ad.phone,  
-    ad.email,  
-    DATE(ad.created_at) AS created_at,  
-    COUNT(o.id) AS total_orders, 
-	COUNT(w.id) AS total_wishlists 
+    ad.email, 
+    COALESCE(w.total_wishlists, 0) AS wishlists,
+	COUNT(DISTINCT o.id) AS total_orders,  
+	ROUND(COALESCE(SUM(od.quantity), 0)) AS total_quantity,
+	FORMAT(ROUND(COALESCE(SUM(o.grand_total), 0)), 2) AS total_amount,
+	DATE(ad.created_at) AS created_at
 FROM  
     bbbd_ecommerce_test.addresses ad  
 LEFT JOIN  
     bbbd_ecommerce_test.orders o ON ad.id = o.user_id  
 LEFT JOIN  
-    bbbd_ecommerce_test.wishlists w ON ad.id = w.user_id
-	
+    bbbd_ecommerce_test.order_details od ON o.id = od.order_id  
+LEFT JOIN  
+    (SELECT user_id, COUNT(*) AS total_wishlists 
+     FROM bbbd_ecommerce_test.wishlists 
+     GROUP BY user_id) w ON ad.id = w.user_id  
 GROUP BY  
-    ad.id, ad.email, ad.phone  
+    ad.id  
 ORDER BY  
-    created_at DESC;
+    total_orders DESC
+LIMIT 10;
 	
+----------------------------------------------------- Customers List By Date Wise------------------------------------
 	
-	
-	SELECT  
+SELECT  
     ad.id AS customer_id,  
     MIN(ad.name) AS customer_name,  
     ad.phone,  
-    ad.email,  
-    DATE(ad.created_at) AS created_at,  
-    COUNT(o.id) AS total_orders
+    ad.email, 
+    COALESCE(w.total_wishlists, 0) AS wishlists,
+	COUNT(DISTINCT o.id) AS total_orders,  
+	ROUND(COALESCE(SUM(od.quantity), 0)) AS total_quantity,
+	FORMAT(ROUND(COALESCE(SUM(o.grand_total), 0)), 2) AS total_amount,
+	DATE(ad.created_at) AS created_at
 FROM  
     bbbd_ecommerce_test.addresses ad  
 LEFT JOIN  
-    bbbd_ecommerce_test.orders o ON ad.id = o.user_id 
+    bbbd_ecommerce_test.orders o ON ad.id = o.user_id  
+LEFT JOIN  
+    bbbd_ecommerce_test.order_details od ON o.id = od.order_id  
+LEFT JOIN  
+    (SELECT user_id, COUNT(*) AS total_wishlists 
+     FROM bbbd_ecommerce_test.wishlists 
+     GROUP BY user_id) w ON ad.id = w.user_id  
+ WHERE DATE(ad.created_at) BETWEEN '2025-03-013' AND '2025-03-13'
 GROUP BY  
-    ad.id, ad.email, ad.phone  
+    ad.id  
 ORDER BY  
-    created_at DESC;
+    total_orders DESC
+LIMIT 10;
